d7369f9c3346322000f03d48f20c35e423ccd52a
devRaxmanov <raxmanovjorabek5@gmail.com>
2023-09-25 11:28:05 +0500

    scriptJs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 "use strict"
 window.addEventListener("DOMContentLoaded", ()=>{
-    
+    let stateSymbols = document.querySelectorAll('.headerTop .uzbInfo .item .infoItem');
+
+    stateSymbols.forEach((item)=>{
+        item.addEventListener('mouseenter', ()=>{
+            item[0].classList.add('active')
+        })
+    })
+
+
 })
