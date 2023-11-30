ae69577ca967447bf72c7cb7ecbdd0110b41307e
devRaxmanov <raxmanovjorabek5@gmail.com>
2023-11-30 13:58:31 +0500

    new ones

--- js/script.js
+++ js/script.js
@@ -108,5 +108,25 @@ window.addEventListener("DOMContentLoaded", ()=>{
             toHemis.style.display = 'none'
         })
 
+        const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
 
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+        });
 })
